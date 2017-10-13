7c53fe9ce3230838ddd6cef3f99db9fe12ed0463
keligeri <kel.gergo@gmail.com>
2017-10-13 12:20:50 +0200

    Added basic verbs method, invoke these instead of Repo layer

--- src/main/java/com/codecool/spring/rest/service/AddressService.java
+++ src/main/java/com/codecool/spring/rest/service/AddressService.java
@@ -1,34 +1,54 @@
 package com.codecool.spring.rest.service;
 
 import com.codecool.spring.rest.model.Address;
+import com.codecool.spring.rest.model.Person;
 import com.codecool.spring.rest.repository.AddressRepository;
+import com.codecool.spring.rest.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class AddressService {
 
     private final AddressRepository addressRepository;
-    private final PersonService personService;
 
     @Autowired
-    public AddressService(AddressRepository addressRepository, PersonService personService) {
+    public AddressService(AddressRepository addressRepository) {
         this.addressRepository = addressRepository;
-        this.personService = personService;
     }
 
-    public void delete(Address address) {
-        deleteDependency(address);
+    public Address findById(long id) {
+        return addressRepository.findOne(id);
+    }
+
+    public Iterable<Address> findAll(){
+        return addressRepository.findAll();
+    }
+
+    public void save(Address address) {
+        Set<Person> persons = address.getPersons();
+        persons.forEach(person -> person.setAddress(address));
+        addressRepository.save(address);
+    }
+
+    public void delete(long id) {
+        Address address = addressRepository.findOne(id);
+
+        address.getPersons().forEach(person -> person.setAddress(null));
         addressRepository.delete(address);
     }
 
-    private void deleteDependency(Address address) {
-        personService.deleteAddress(address.getPersons());
-        address.setPersons(null);
-    }
-
+    // Doesn't work well
     public void update(long id, Address address) {
+//        Address prevAddress = addressRepository.findOne(id);
+//        prevAddress.getPersons().forEach(person -> person.setAddress(null));    // person still exists without address
+
         address.setId(id);
+        address.getPersons().forEach(person -> person.setAddress(address));
+        address.getPersons().forEach(person -> System.out.println(person.getName()));
+
         addressRepository.save(address);
     }
 
