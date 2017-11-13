d3304e050ff7923d6e23db1caa931458f532628c
keligeri <kel.gergo@gmail.com>
2017-11-13 13:40:52 +0100

    Extended DataLoader with userrepository to save user too

--- src/main/java/com/codecool/spring/rest/utils/DataLoader.java
+++ src/main/java/com/codecool/spring/rest/utils/DataLoader.java
@@ -2,8 +2,12 @@ package com.codecool.spring.rest.utils;
 
 import com.codecool.spring.rest.model.Address;
 import com.codecool.spring.rest.model.Person;
+import com.codecool.spring.rest.model.Role;
+import com.codecool.spring.rest.model.User;
 import com.codecool.spring.rest.repository.AddressRepository;
 import com.codecool.spring.rest.repository.PersonRepository;
+import com.codecool.spring.rest.repository.RoleRepository;
+import com.codecool.spring.rest.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
@@ -14,20 +18,33 @@ public class DataLoader implements ApplicationRunner {
 
     private final AddressRepository addressRepository;
     private final PersonRepository personRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
 
     @Autowired
-    public DataLoader(AddressRepository addressRepository, PersonRepository personRepository) {
+    public DataLoader(AddressRepository addressRepository, PersonRepository personRepository, UserRepository userRepository, RoleRepository roleRepository) {
         this.addressRepository = addressRepository;
         this.personRepository = personRepository;
+        this.userRepository = userRepository;
+        this.roleRepository = roleRepository;
     }
 
     @Override
     public void run(ApplicationArguments applicationArguments) throws Exception {
-        Person gergo = new Person("Gergő", 24, new Address(8900L, "Zalaegerszeg"));
-        Person pisti = new Person("Pista", 20, new Address(1146L, "Budapest"));
+        Role adminRole = new Role("ROLE_ADMIN");
+        Role userRole = new Role("ROLE_USER");
+
+        User admin = new User("admin", "admin", adminRole);
+        User user = new User("user", "user", userRole);
+        userRepository.save(admin);
+        userRepository.save(user);
 
         Address zeg = new Address(8900L, "Zalaegerszeg");
         Address pest = new Address(1146L, "Budapest");
+
+        Person gergo = new Person("Gergő", 24, zeg, admin);
+        Person pisti = new Person("Pista", 20, pest, user);
+
 //        addressRepository.save(zeg);
 //        addressRepository.save(pest);
 
