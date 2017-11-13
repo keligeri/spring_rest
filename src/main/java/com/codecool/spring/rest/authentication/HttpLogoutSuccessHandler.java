d86d0200f80fa7762125ebdaf14cc2160867709b
keligeri <kel.gergo@gmail.com>
2017-11-13 15:04:49 +0100

    Add component annotation and rename file

--- src/main/java/com/codecool/spring/rest/authentication/HttpLogoutSuccessHandler.java
+++ src/main/java/com/codecool/spring/rest/authentication/HttpLogoutSuccessHandler.java
@@ -2,16 +2,15 @@ package com.codecool.spring.rest.authentication;
 
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.stereotype.Component;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-/**
- * Created by keli on 2017.11.13..
- */
-public class HttpLogoutSuccesHandler implements LogoutSuccessHandler{
+@Component
+public class HttpLogoutSuccessHandler implements LogoutSuccessHandler{
 
     @Override
     public void onLogoutSuccess(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse,
