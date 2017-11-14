26931d1f84f18236f67274fecd3e85f585fdfe11
keligeri <kel.gergo@gmail.com>
2017-11-14 15:58:17 +0100

    add securityConfig with some beans and custm authenticationProvider

--- src/main/java/com/codecool/spring/rest/config/SecurityConfig.java
+++ src/main/java/com/codecool/spring/rest/config/SecurityConfig.java
@@ -1,39 +1,106 @@
 package com.codecool.spring.rest.config;
 
+import com.codecool.spring.rest.authentication.AuthFailureHandler;
+import com.codecool.spring.rest.authentication.AuthSuccessHandler;
+import com.codecool.spring.rest.authentication.HttpAuthenticationEntryPoint;
+import com.codecool.spring.rest.authentication.HttpLogoutSuccessHandler;
+import com.codecool.spring.rest.service.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.*;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
-@EnableGlobalMethodSecurity
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final UserDetailsServiceImpl userDetailsService;
+    private final HttpAuthenticationEntryPoint authenticationEntryPoint;
+    private final AuthSuccessHandler successHandler;
+    private final AuthFailureHandler failureHandler;
+    private final HttpLogoutSuccessHandler logoutSuccessHandler;
+    private final BCryptPasswordEncoder passwordEncoder;
+
     @Autowired
-    public void configureAuth(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("admin")
-                .password("admin")
-                .roles("ADMIN")
-            .and()
-                .withUser("user")
-                .password("user")
-                .roles("USER");
+    public SecurityConfig(UserDetailsServiceImpl userDetailsService, HttpAuthenticationEntryPoint authenticationEntryPoint, AuthSuccessHandler successHandler, AuthFailureHandler failureHandler, HttpLogoutSuccessHandler logoutSuccessHandler, BCryptPasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.authenticationEntryPoint = authenticationEntryPoint;
+        this.successHandler = successHandler;
+        this.failureHandler = failureHandler;
+        this.logoutSuccessHandler = logoutSuccessHandler;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    public UserDetailsService userDetailsServiceBean() throws Exception {
+        return super.userDetailsServiceBean();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return authenticationProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        AuthenticationProvider authenticationProvider = authenticationProvider();
+        auth.authenticationProvider(authenticationProvider);
+    }
+
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-            .antMatchers("/person/**").permitAll()
-            .antMatchers("/address/**").hasRole("ADMIN")
-            .anyRequest().authenticated()
-        .and().httpBasic()
-        .and()
-            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/person/**").permitAll()
+                .antMatchers("/address/**").hasRole("ADMIN")
+            .and()
+                .authenticationProvider(authenticationProvider())
+                .exceptionHandling()
+                .authenticationEntryPoint(authenticationEntryPoint)
+            .and()
+                .formLogin()
+                .permitAll()
+                .loginProcessingUrl("/login/")
+                .usernameParameter("username")
+                .passwordParameter("password")
+                .successHandler(successHandler)
+                .failureHandler(failureHandler)
+            .and()
+                .logout()
+                .permitAll()
+                .logoutRequestMatcher(new AntPathRequestMatcher("/login", "DELETE") )
+                .logoutSuccessHandler(logoutSuccessHandler)
+            .and()
+                .sessionManagement()
+                .maximumSessions(1);
+
+        http.authorizeRequests().anyRequest().permitAll();
     }
 }
